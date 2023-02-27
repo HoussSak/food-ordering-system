a89e32b7bc8671bcfb1a6073ce6b4be72e550718
HoussSak <99645587+HoussSak@users.noreply.github.com>
2023-02-27 15:37:44 +0100

    Maj

--- order-service/order-domain/order-domain-core/src/main/java/com/food/odrering/system/order/service/domain/valueobject/OrderItemId.java
+++
@@ -1,9 +0,0 @@
-package com.food.odrering.system.order.service.domain.valueobject;
-
-import com.food.ordering.system.domain.valueobject.BaseId;
-
-public class OrderItemId extends BaseId<Long> {
-    public OrderItemId(Long value) {
-        super(value);
-    }
-}
